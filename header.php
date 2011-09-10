c8ce5d5d8a885a1eaf28960475734ce7714bf40c
Oliver Caldwell <oliver@flowdev.co.uk>
2011-09-10 21:09:23 +0100

    Added the title tag to the header

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang='en'>
 	<head>
+		<title><?php the_title(); ?></title>
 		<meta name='author' content='Oliver Caldwell' />
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 		<!--[if lt IE 9]><script type='text/javascript' src='<?php bloginfo('template_url'); ?>/assets/javascript/html5.js'></script><![endif]-->
