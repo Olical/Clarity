ed1cc1563128f1dddb7fa964fe0ab9864d91dbc7
Oliver Caldwell <oliver@flowdev.co.uk>
2011-09-10 18:57:18 +0100

    Styled posts

--- index.php
+++ index.php
@@ -1,2 +1,41 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<?php
+	get_header();
+	
+	if (have_posts())
+	{
+		while (have_posts())
+		{
+			the_post();
+			
+			?>
+				<article>
+					<h3 class='post-title'><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h3>
+					
+					<section class='post'>
+						<?php the_content('Read more &rarr;'); ?>
+					</section>
+					
+					<section class='post-meta'>
+						<p>Posted on <?php the_time('F jS, Y'); ?> at <?php the_time('g:ia'); ?> by <?php
+							the_author_posts_link();
+							if (get_the_category())
+							{
+								echo ' under ';
+								the_category(', ');
+							}
+						?>.<br /><?php the_tags('Tagged with ', ', ', '.'); ?></p>
+					</section>
+				</article>
+			<?php
+		}
+	}
+	else
+	{
+		?>
+			<h3>No posts where found.</h3>
+			<p>Sorry, no posts matched your criteria. Please check the URL or try searching for what you require.</p>
+		<?php
+	}
+	
+	get_footer();
+?>
