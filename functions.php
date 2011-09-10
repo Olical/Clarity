463dd894f7c5f7cae2c38cab6a0d888691f3ff40
Oliver Caldwell <oliver@flowdev.co.uk>
2011-09-10 16:30:42 +0100

    Fixed the script registering, scripts are not styles >_> and removed all of the meta data from wp_head();

--- functions.php
+++ functions.php
@@ -20,12 +20,27 @@ function clarity_theme_init()
 	wp_register_style('clarity_theme_styles', $template_url . '/style.css', array('skeleton'));
 	
 	// Register scripts
-	wp_register_style('mootools', $template_url . '/assets/javascript/mootools.js', false, '1.4.0', true);
-	wp_register_style('clarity_theme_scripts', $template_url . '/assets/javascript/clarity.js', array('mootools'));
+	wp_register_script('mootools', $template_url . '/assets/javascript/mootools.js', false, '1.4.0', true);
+	wp_register_script('clarity_theme_scripts', $template_url . '/assets/javascript/clarity.js', array('mootools'), false, true);
 	
 	// Enqueue styles
 	wp_enqueue_style('clarity_theme_styles');
 	
 	// Enqueue scripts
 	wp_enqueue_script('clarity_theme_scripts');
+	
+	// Remove all of the random crap WordPress dumps in the header
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'index_rel_link');
+	remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'rel_canonical');
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+	remove_action('wp_head', 'index_rel_link');
+	remove_action('wp_head', 'feed_links_extra', 3);
+	remove_action('wp_head', 'feed_links', 2);
+	remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+	remove_action('wp_head', 'start_post_rel_link', 10, 0);
+	remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+	wp_deregister_script('l10n');
 }
