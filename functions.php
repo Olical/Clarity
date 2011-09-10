e40bb8d75773fac9ae90d5813837b038b3af020a
Oliver Caldwell <oliver@flowdev.co.uk>
2011-09-10 16:09:11 +0100

    Added functions and loaded all scripts / styles through it

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+// Add action hooks
+add_action('init', 'clarity_theme_init');
+
+/**
+ * Initialises the theme
+ * Enqueues all scripts and styles
+ */
+function clarity_theme_init()
+{
+	// Grab the template URL
+	$template_url = get_bloginfo('template_url');
+	
+	// Register styles
+	wp_register_style('skeleton', $template_url . '/assets/css/skeleton.css', false, '1.1');
+	wp_register_style('clarity_theme_styles', $template_url . '/style.css', array('skeleton'));
+	
+	// Register scripts
+	wp_register_style('mootools', $template_url . '/assets/javascript/mootools.js', false, '1.4.0', true);
+	wp_register_style('clarity_theme_scripts', $template_url . '/assets/javascript/clarity.js', array('mootools'));
+	
+	// Enqueue styles
+	wp_enqueue_style('clarity_theme_styles');
+	
+	// Enqueue scripts
+	wp_enqueue_style('clarity_theme_scripts');
+}
--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 		<meta name='author' content='Oliver Caldwell' />
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 		<!--[if lt IE 9]><script type='text/javascript' src='<?php bloginfo('template_url'); ?>/assets/javascript/html5.js'></script><![endif]-->
-		<link rel='stylesheet' type='text/css' href='<?php bloginfo('template_url'); ?>/assets/css/skeleton.css' />
-		<link rel='stylesheet' type='text/css' href='<?php bloginfo('template_url'); ?>/style.css' />
 		<?php wp_head(); ?>
 	</head>
 	
